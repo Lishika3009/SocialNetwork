836c45af956823ec5af0b37dfeb26c6ff1b4b3c1
Avishi2511 <avishimittal2004@gmail.com>
2024-07-20 20:16:28 +0530

    mongoDB_connected

--- server/index.js
+++ server/index.js
@@ -2,18 +2,27 @@ const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const app = express();
+const userRoutes = require("./routes/userRoutes")
 const socket = require("socket.io");
 require("dotenv").config();
+
 app.use(cors());
 app.use(express.json());
+
+app.use("/api/auth", userRoutes)
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("DB Connetion Successfull");
+    console.log("DB Connection Sucessfull");
   })
   .catch((err) => {
     console.log(err.message);
   });
+
+const server = app.listen(process.env.PORT, ()=>{
+  console.log(`Server started on Port ${process.env.PORT}`);
+});
