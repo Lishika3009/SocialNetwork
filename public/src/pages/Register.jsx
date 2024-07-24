211a6484a572e2e5e084d0e8fbc22c9cb2d8b891
Avishi2511 <avishimittal2004@gmail.com>
2024-07-24 13:27:19 +0530

    register_to_login

--- public/src/pages/Register.jsx
+++ public/src/pages/Register.jsx
@@ -25,7 +25,7 @@ export default function Register() {
 
   useEffect(() => {
     if (localStorage.getItem(process.env.REACT_APP_LOCALHOST_KEY)) {
-      navigate("/");
+      navigate("/login");
     }
   }, []);
 
@@ -79,7 +79,7 @@ export default function Register() {
           process.env.REACT_APP_LOCALHOST_KEY,
           JSON.stringify(data.user)
         );
-        navigate("/");
+        navigate("/login");
       }
     }
   };
